6733be393f09f728b7951720569a8f2b31769a28
residenttrickster <metkusblackus@gmail.com>
2020-06-14 13:25:22 -0400

    all User controllers created, Location menu works, Story controllers - half

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,2 +1,33 @@
 class StoriesController < ApplicationController
+
+    def index
+        @stories = Story.all  
+        render :index 
+    end
+
+    def show
+        @story = story_find
+        render :show
+    end 
+
+    def new
+        @story = Story.new
+        render :new 
+    end 
+
+    def create
+        @story = Story.create(story_params)
+        render_to story_path(@story)
+    end
+
+    private
+
+    def story_find
+        Story.find(params[:id])
+    end 
+
+    def story_params
+        params.require(:story).permit(:title, :body)
+    end 
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,50 @@
 class UsersController < ApplicationController
+
+    def index 
+        @users = User.all
+        render :index 
+    end 
+
+    def show
+        @user = user_find
+        render :show 
+    end 
+
+    def new
+        @user = User.new
+        render :new 
+    end
+
+    def create
+        @user = User.create(user_params)
+        redirect_to user_path(@user)
+    end 
+
+    def edit
+        @user = user_find
+        render :edit 
+    end 
+
+    def update
+        @user = user_find
+        @user.update(user_params)
+        redirect_to user_path(@user)
+    end 
+
+    def destroy
+        @user = user_find
+        @user.destroy
+        redirect_to users_path
+    end 
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :bio)
+    end 
+
+    def user_find
+        User.find(params[:id])
+    end 
+
 end
